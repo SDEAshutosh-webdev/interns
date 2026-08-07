2812bf3211d5a87a9308117f5e9446582f8e8ff5
Snehal Dhage <snehaldhage22@gmail.com>
2026-08-07 10:45:30 +0530

    Resolve merge conflicts

--- interns/Pract/src/App.jsx
+++ interns/Pract/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+import Button from "./Common/Button";
+import ButtonRoutes from "./routes/ButtonRoutes";
 import ProductRoutes from './routes/ProductRoutes';
+import './App.css'
 
 function App() {
-  return <ProductRoutes />;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <ProductRoutes />;
+      <ButtonRoutes />;
+      <div className="button-container">
+      <Button text="Save" variant="primary"/>
+      </div>
+    
+    </>
+  )
 }
 
 export default App;
+
+
