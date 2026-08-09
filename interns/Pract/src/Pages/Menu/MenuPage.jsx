8ff5482e8c918b3050a1490f08aebcb5dbbe906e
VedMisar <vedmisar30@gmail.com>
2026-08-09 14:36:33 +0530

    Added CategoryFilter.jsx feature by ved

--- interns/Pract/src/Pages/Menu/MenuPage.jsx
+++ interns/Pract/src/Pages/Menu/MenuPage.jsx
@@ -1,16 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FoodCard from "../../components/features/food/FoodCard";
+import { foodService } from "../../api/foodService";
+import CategoryFilter from "../../components/features/food/CategoryFilter";
 import SearchBar from "../../components/common/SearchBar/SearchBar";
 import { useFoodItems } from "../../hooks/useFoodItems";
 import "../../styles/MenuPage.css";
+
 
 const CATEGORIES = ["All", "Breakfast", "Main Course", "Snacks", "Italian"];
 
 function MenuPage() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [search, setSearch] = useState("");
+  const [allFoodItems, setAllFoodItems] = useState([]);
   const { foodItems, loading } = useFoodItems(selectedCategory);
 
+  useEffect(() => {
+    foodService.getAllFoodItems().then((data) => {
+      setAllFoodItems(data);
+    });
+  }, []);
+
+  // calculate how many items are in the each category
+const categoryCounts = {
+  All: allFoodItems.length,
+
+  Breakfast: allFoodItems.filter(
+    (item) => item.category === "Breakfast"
+  ).length,
+
+  "Main Course": allFoodItems.filter(
+    (item) => item.category === "Main Course"
+  ).length,
+
+  Snacks: allFoodItems.filter(
+    (item) => item.category === "Snacks"
+  ).length,
+
+  Italian: allFoodItems.filter(
+    (item) => item.category === "Italian"
+  ).length,
+};
   const filteredItems = foodItems.filter(
     (item) =>
       item.title.toLowerCase().includes(search.toLowerCase()) ||
@@ -26,19 +56,12 @@ function MenuPage() {
           <SearchBar search={search} setSearch={setSearch} placeholder="Search by name or ingredient..." />
         </div>
       </div>
-
-      {/* Category Pills */}
-      <div className="category-pills">
-        {CATEGORIES.map((cat) => (
-          <button
-            key={cat}
-            className={`pill ${selectedCategory === cat ? "active" : ""}`}
-            onClick={() => setSelectedCategory(cat)}
-          >
-            {cat}
-          </button>
-        ))}
-      </div>
+    <CategoryFilter
+      categories={CATEGORIES}
+      selectedCategory={selectedCategory}
+      onCategoryChange={setSelectedCategory}
+      categoryCounts={categoryCounts}
+    />
 
       {/* Food Items Grid */}
       <div className="menu-container">
