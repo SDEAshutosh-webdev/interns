07cf12e48bb3abaacd9f70ea266f00cce4445f24
Rina Pawar <rinapawar162@gmail.com>
2026-08-09 21:02:59 +0530

    Add footer and dark mode

--- interns/Pract/src/App.jsx
+++ interns/Pract/src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import Navbar from "./components/common/Navbar/Navbar";
+import Footer from "./components/common/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import { CartProvider } from "./context/CartProvider";
-import "./styles/App.css";
+import "./Styles/App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <CartProvider>
-      <div className="app-container">
+      <div className={darkMode ? "app-container dark-mode" : "app-container"}>
         <Navbar />
+
         <main className="main-content">
           <AppRoutes />
         </main>
+
+        <Footer
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
       </div>
     </CartProvider>
   );
