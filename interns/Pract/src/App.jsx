e41705fe2cb905a1fac152cba6b9e063eba8cebd
Rutuja Patilkhede <rutujapatilkhede74@gmail.com>
2026-08-10 20:19:48 +0530

    Added Wishlist Feature

--- interns/Pract/src/App.jsx
+++ interns/Pract/src/App.jsx
@@ -1,17 +1,21 @@
 import Navbar from "./components/common/Navbar/Navbar";
 import AppRoutes from "./routes/AppRoutes";
 import { CartProvider } from "./context/CartProvider";
+import { WishlistProvider } from "./context/WishlistContext";
 import "./styles/App.css";
 
 function App() {
   return (
     <CartProvider>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <AppRoutes />
-        </main>
-      </div>
+      <WishlistProvider>
+        <div className="app-container">
+          <Navbar />
+
+          <main className="main-content">
+            <AppRoutes />
+          </main>
+        </div>
+      </WishlistProvider>
     </CartProvider>
   );
 }
